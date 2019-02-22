2118e85daa265f2debc8504caeb3e32a31a882d4
yaoharry <harryyao13@gmail.com>
2019-02-21 17:07:58 -0800

    Initial and working reactjs integration with sailjs

---
+++ MM-PulpFree/app/assets/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = () => {
+  return (
+    <div>
+      Simple Sails-React stater
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
