08bd0e836682b52fd72b885870a0eb976174c7a3
MuchToKnow <justindc99edu@gmail.com>
2019-02-24 01:34:14 -0800

    Fixed error handling

--- MM-PulpFree/app/actions/auth.js
+++ MM-PulpFree/app/actions/auth.js
@@ -18,27 +18,20 @@ const signup = async (body) => {
   const hashedPass = await hashPass(body.pass)
 
   //Get db client from client pool
-  pool.connect((err, client, done) => {
-    if (err) {
-      console.log("Error connecting to client pool " + err)
-      throw Error("Error connecting to client pool " + err)
-    }
+  const client = await pool.connect()
+  try {
     //Create user
     const d = new Date()
     const epochSeconds = Math.round(d.getTime() / 1000)
     const dbArr = [body.email, hashedPass, body.country, body.lang, body.firstname, body.lastname, epochSeconds]
-    client.query('INSERT INTO users(email, pass, country, lang, firstname, lastname, lastlogin) VALUES($1, $2, $3, $4, $5, $6, $7)', dbArr, (err, res) => {
-      done()
-      if (err) {
-        console.log("Error inserting new user: " + err)
-      }
-      else {
-        console.log(res.rows[0])
-        return res.rows[0]
-      }
-    })
-  })
+    const res = await client.query('INSERT INTO users(email, pass, country, lang, firstname, lastname, lastlogin) VALUES($1, $2, $3, $4, $5, $6, $7)', dbArr)
+    return (JSON.stringify(res))
+  } finally {
+    client.release()
+  }
 }
+
+
 
 module.exports = {
   signup
--- MM-PulpFree/app/routes/auth.js
+++ MM-PulpFree/app/routes/auth.js
@@ -1,15 +1,15 @@
 const app = module.exports = require('express')();
 
-const {signup} = require('../actions').auth
+const { signup } = require('../actions').auth
 
 app.post('/signup', (req, res) => {
   signup(req.body)
     .then((user) => res.send({
-        user
-      })
+      msg: "User created successfully!"
+    })
     )
     .catch((err) => {
       console.log(err.stack)
-      res.status(400).send({msg: "Signup failed", err: err.message})
+      res.status(400).send(err)
     })
 })
