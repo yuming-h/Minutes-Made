6429c5554888ac49f1816141ceca04c217459d0d
MuchToKnow <justindc99edu@gmail.com>
2019-02-24 01:43:43 -0800

    Cleanup and add function comments

--- MM-PulpFree/app/routes/auth.js
+++ MM-PulpFree/app/routes/auth.js
@@ -4,12 +4,11 @@ const { signup } = require('../actions').auth
 
 app.post('/signup', (req, res) => {
   signup(req.body)
-    .then((user) => res.send({
+    .then(() => res.send({
       msg: "User created successfully!"
     })
     )
     .catch((err) => {
-      console.log(err.stack)
       res.status(400).send(err)
     })
 })
--- MM-PulpFree/app/utils/hashPass.js
+++ MM-PulpFree/app/utils/hashPass.js
@@ -1,5 +1,9 @@
 const bcrypt = require('bcrypt')
 
+/**
+ * Returns the hashed password async.
+ * @param String pass 
+ */
 const hashPass = async (pass) => {
   const saltRounds = 12
   return await bcrypt.hash(pass, saltRounds)
