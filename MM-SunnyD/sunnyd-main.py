d53ad19430374701557719232365c4e496cf0a7e
Harry Yao <harryyao13@gmail.com>
2019-04-06 14:35:16 -0700

    Added optional keys to db writer

--- MM-SunnyD/sunnyd-main.py
+++ MM-SunnyD/sunnyd-main.py
@@ -23,6 +23,7 @@ async def transcripts_add():
     """Adds a transcript to the MongoDB database"""
     data = await request.get_json()
     if check_transcript_request(data):
+        data = append_missing_optionals(data)
         app.mongo.db.transcripts.insert_one(data)
         return jsonify({'success': True, 'message': 'Transcript successfully added'}), 200
     return jsonify({'success': False, 'message': 'Bad request parameters'}), 400
@@ -35,6 +36,20 @@ def check_transcript_request(data):
         if data.get(key, None) is None:
             valid_response = False
     return valid_response
+
+def append_missing_optionals(data):
+    """Appends missing optional keys to request"""
+    optional_keys = ['speaker_id', 'speaker_name']
+    optional_arr_keys = ['action_items']
+    for key in optional_keys:
+        if data.get(key, None) is None:
+            data[key] = ''
+    for key in optional_arr_keys:
+        if data.get(key, None) is None:
+            data[key] = []
+    print (data)
+
+    return data
 
 if __name__ == "__main__":
     app.run()
